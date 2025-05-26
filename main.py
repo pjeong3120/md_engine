d5650afa8dab09216f027e70ffeba7fa78cb621e
Phillip Jeong <pjeong3120@gmail.com>
2025-05-26 13:03:16 -0700

    Energy conservation success

--- main.py
+++ main.py
@@ -29,16 +29,16 @@ if __name__ == '__main__':
 
     # screen through dt - energy.std() = dt^2
     total_time = 10
-    for i, dt in enumerate([0.1, 0.01, 0.001, 0.0001, 0.00001]):
+    for i, dt in enumerate([0.0001, 0.0002, 0.0004, 0.0006, 0.0008, 0.0010]):
         r = r0.copy()
         v = v0.copy()
         num_steps = int(total_time / dt)
         save_every = max(1, num_steps // 1000) # Save 1000 frames total
 
         engine = VerletEngine(r, v, masses, lj_potential, dt, unit_cell)
-        job_name = f'N={int(r.shape[0])}_totaltime={int(total_time)}_dt=1e{int(np.log10(dt))}'
+        job_name = f'N={int(r.shape[0])}_totaltime={int(total_time)}_dt={i*2}e-4'
         data = engine.run(num_steps, save_every, job_name = job_name)
 
         # Visualize Data 
-        frame_paths = visualize_trajectory(job_path=f'./runs/{job_name}', visualize_up_to = 101) # Visualize only 101 frames for runtime
+        frame_paths = visualize_trajectory(job_path=f'./runs/{job_name}', visualize_up_to = 101) # Visualize only 101 frames for runtime aka only the first 1 s
         make_gif(frame_paths, job_path = f'./runs/{job_name}')
