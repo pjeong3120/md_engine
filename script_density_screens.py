6f3452593e2f9441aceab843891fb1e35c41cd00
Phillip Jeong <pjeong3120@gmail.com>
2025-06-13 00:45:24 -0700

    Various plotting stuff - no real changes in functionality

--- script_density_screens.py
+++ script_density_screens.py
@@ -14,6 +14,38 @@ if __name__ == '__main__':
     # Parameterize potential
     r_max = 5.0
     sigma = 1.0
+    epsilon = 1
+    lj_potential = LennardJones(sigma, epsilon, unit_cell, r_max)
+    
+    # Other engine params
+    dt = 0.001
+    num_steps = int(10 / dt) # simulate for 10 seconds
+    save_every = max(1, num_steps // 1000) # Save 1000 frames total
+    gamma = 1.0
+
+
+    for N in [20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 115, 125]:
+        masses = np.ones((N,))
+        r, v = initialize_n_particles_target_temp_2d(N, masses, target_temp, unit_cell)
+
+        engine = CanonicalVerletEngine(r, v, masses, lj_potential, dt, unit_cell, target_temp, gamma)
+        
+        job_name = f'density_screens_eps={epsilon}/N={int(N)}'
+        data = engine.run(num_steps, save_every, job_name = job_name)
+
+        # Visualize Data 
+        frame_paths = visualize_trajectory(job_path=f'./runs/{job_name}', visualize_up_to = 101) # Visualize only 101 frames for runtime aka only the first 1 s
+        make_gif(frame_paths, job_path = f'./runs/{job_name}')
+
+    np.random.seed(42)
+    
+    # Initial lattice params
+    target_temp = 5
+    unit_cell = np.array([10, 10])
+
+    # Parameterize potential
+    r_max = 5.0
+    sigma = 1.0
     epsilon = 10
     lj_potential = LennardJones(sigma, epsilon, unit_cell, r_max)
     
@@ -24,15 +56,16 @@ if __name__ == '__main__':
     gamma = 1.0
 
 
-    for N in [16, 25, 36, 49, 64, 81, 85, 90, 95, 100, 105, 110, 115, 121]:
+    for N in [20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 115, 125]:
         masses = np.ones((N,))
         r, v = initialize_n_particles_target_temp_2d(N, masses, target_temp, unit_cell)
 
         engine = CanonicalVerletEngine(r, v, masses, lj_potential, dt, unit_cell, target_temp, gamma)
         
-        job_name = f'density_screens/N={int(N)}'
+        job_name = f'density_screens_eps={epsilon}/N={int(N)}'
         data = engine.run(num_steps, save_every, job_name = job_name)
 
         # Visualize Data 
         frame_paths = visualize_trajectory(job_path=f'./runs/{job_name}', visualize_up_to = 101) # Visualize only 101 frames for runtime aka only the first 1 s
         make_gif(frame_paths, job_path = f'./runs/{job_name}')
+
